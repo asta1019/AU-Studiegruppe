0ed7a27912c54b8d39468b218d30dc6bf0c6081c
asta1019 <asta@ovonbach.dk>
2025-08-25 16:11:28 +0200

    Update BankApp.java

--- 5.semetser/SWEA/Prerequisites exercises/src/BankApp.java
+++ 5.semetser/SWEA/Prerequisites exercises/src/BankApp.java
@@ -1,7 +1,24 @@
-public class BankApp{
+public class BankApp {
     public static void main(String[] args) {
+
+        // Lav konto
         BankAccount account1 = new BankAccount(1000);
         BankAccount account2 = new BankAccount(1500);
         BankAccount account3 = new BankAccount(2000);
+
+        // Chack balance
+        System.out.println("Acc1 has balance: " + account1.CheckBalance());
+
+        // Insert
+        account1.insert(200);
+
+        System.out.println("Acc1 has balance: " + account1.CheckBalance());
+
+        // Withdraw
+        account1.withdraw(100);
+
+        System.out.println("Acc1 has balance: " + account1.CheckBalance());
+
     }
 }
+
